1e798dc786ba19352c3d6232508a4b9b477c3964
Cedrick Oka Baidai <okacedrick@gmail.com>
2021-06-19 10:21:40 +0000

    Sanitize

--- docker/docker-symfony-entrypoint.sh
+++ docker/docker-symfony-entrypoint.sh
@@ -5,29 +5,29 @@ printenv | sed 's/^\([^=]*\)\=\(.*\)$/\1\="\2"/g' | sed 's/^\(_\)\=\(.*\)$//g' >
 
 if [ "${APP_ENV}" != "test" ]; then
 	## Decomments here if database configuration is necessary
-	#php /app/bin/console doctrine:database:create --if-not-exists && \
-	#php /app/bin/console doctrine:schema:update --force && \
+	#php /app/bin/console doctrine:database:create --if-not-exists
+	#php /app/bin/console doctrine:schema:update --force
 	
 	## Install the bundle assets
-	php /app/bin/console assets:install public --symlink --relative && \
+	php /app/bin/console assets:install public --symlink --relative
 	
 	## Clear env cache
-	php /app/bin/console cache:clear --no-debug && \
+	php /app/bin/console cache:clear --no-debug
 	
 	## Configure php-fpm pool conf
 	#if [ ! -z $(grep "PM_MAX_CHILDREN" "/usr/local/etc/php-fpm.d/z-overrides.conf.template") ]; then 
-	#	export PM_MAX_CHILDREN=${PM_MAX_CHILDREN:-100} PM_MAX_REQUESTS=${PM_MAX_REQUESTS:-200} && \
+	#	export PM_MAX_CHILDREN=${PM_MAX_CHILDREN:-100} PM_MAX_REQUESTS=${PM_MAX_REQUESTS:-200}
 	#	envsubst "$$PM_MAX_CHILDREN $$PM_MAX_REQUESTS" < /usr/local/etc/php-fpm.d/z-overrides.conf.template > /usr/local/etc/php-fpm.d/z-overrides.conf;
 	#fi
 	
 	envsubst < /etc/youtube-dl.conf > /etc/youtube-dl.conf
 	
 	## Add messenger confid and decomments here if supervisor configuration is necessary
-	envsubst < /etc/supervisor.d/messenger.template > /etc/supervisor.d/messenger.ini && \
-	supervisord -c /etc/supervisord.conf && \
+	envsubst < /etc/supervisor.d/messenger.template > /etc/supervisor.d/messenger.ini
+	supervisord -c /etc/supervisord.conf
 	
 	## Decomments here if cron configuration is necessary
-	/usr/sbin/crond -b -l 8 -L /dev/stdout && \
+	/usr/sbin/crond -b -l 8 -L /dev/stdout
 	
 	chown -R www-data:www-data /app/var/
 fi
